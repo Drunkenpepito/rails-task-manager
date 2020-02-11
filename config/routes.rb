3325a21a719b7e06f20e99f542b81433da105b16
pierre furic <pfuric@hotmail.com>
2020-02-11 14:05:23 +0100

    simple CRUD working

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
+
+#          Prefix Verb   URI Pattern                     Controller#Action
+#     restaurants GET    /restaurants(.:format)          restaurants#index
+#                 POST   /restaurants(.:format)          restaurants#create
+#  new_restaurant GET    /restaurants/new(.:format)      restaurants#new
+# edit_restaurant GET    /restaurants/:id/edit(.:format) restaurants#edit
+#      restaurant GET    /restaurants/:id(.:format)      restaurants#show
+#                 PATCH  /restaurants/:id(.:format)      restaurants#update
+#                 DELETE /restaurants/:id(.:format)      restaurants#destroy
